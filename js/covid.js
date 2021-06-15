c0fdf2e2ea2c4eab19f0bb66b025417a15fe0094
abdulnmo <abdulnmo@sheridancollege.ca>
2021-06-14 20:31:49 -0400

    program running

--- js/covid.js
+++ js/covid.js
@@ -30,6 +30,7 @@ function parseVaccine(data){
 }
 $("#DisplayData").click(function(){
     myArray=[];
+   
     console.log(localStorage.key(9));
     for (var i=0; i<localStorage.length;i++){
         console.log(localStorage.key(i));
@@ -37,12 +38,52 @@ $("#DisplayData").click(function(){
 
 
     }
-    console.dir(myArray);
-    alert(9);
+
+    extract(myArray.sort());
+
+
+    
+})
+function extract(params){
+    $('#title').html("Summary of Total Vaccine Doses");
+    $.each(params,(index,user)=>{
+        //works but uses with index
+//        $('#data').append("<li>"+(user)+"<button id='" + ('test'+index) + "' class='vaccine'>Details</button><span></span>"+"</li>");
+//<span id='" + ('t'+index) + "' ></span>"
+$('#data').append("<li>"+(user)+" <br/><span id='" + (user) + "' class='vaccine'>Details</span>"+"</li>");
+   $(".vaccine").css("color", "red");
 })
 
 }
+$('#data').on("click", ".vaccine",function(){
+   //check length to see if data has been appended before repeat
+   //before appending because it keeps appending
+   var b=$(this).attr("id");//id of span
+   //check the value in span to see if has h2 tag with id=addo
+   if ($('#'+b).find('#addo').length==0){
+     $('#'+b).append("<br/><h2 id='addo'>Vaccine Doses Detail sECTION</h2>");
+
+    $('#'+b).append("<br/><p>"+localStorage.getItem(b)+"</p>");
+    $("p").css("color", "orange");
+    $("p").css("text-decoration", "none");
+   }
+ 
+  // var f = $('span#'+b).next().length;
+ 
+  
+  
+
+
+})
+//$('#test0').click(function(){
+ //   alert("lll");
+//})
+}
 );  
+//make it run once once clicked
+
+
+
 
 class grab{
     constructor(id,d,prev_doses,admin,fully,indiv){
