9bcf025d44c695f125adddd96413a620a4eca99f
abdulnmo <abdulnmo@sheridancollege.ca>
2021-06-17 03:15:53 -0400

    items fixed

--- js/covid.js
+++ js/covid.js
@@ -1,20 +1,25 @@
 
 
 $(document).ready(()=>{
-
-    $.ajax({
-        type:"GET",
-        url:"js/vaccine_doses.json",
-        dataType:"json",
-        error:(e) => {console.log(e)},
-        success: parseVaccine
-        
-    });
+  $("#load").click(function(e){
+        $.ajax({
+                type:"GET",
+                url:"js/vaccine_doses.json",
+                dataType:"json",
+                error:(e) => {console.log(e)},
+                success: parseVaccine
+                
+            });
+  })
+    
 
 function parseVaccine(data){
+    alert("Data has been loaded");
   //console.dir(data);
-   for (let i of data){
-       let item=new grab(("991590042"),(i.report_date),(i.previous_day_doses_administered),
+  if (localStorage.length== 0){
+     
+      for (let i of data){
+       let item=new grab(("abdulnmo"),(i.report_date),(i.previous_day_doses_administered),
         (i.total_doses_administered),
         (i.total_doses_in_fully_vaccinated_individuals),
         (i.total_individuals_fully_vaccinated));
@@ -22,7 +27,9 @@ function parseVaccine(data){
         localStorage.setItem(item.d,JSON.stringify(item));
 
     }
-    alert("Done");
+    
+  }
+   
 
 
 
@@ -30,42 +37,56 @@ function parseVaccine(data){
 }
 $("#DisplayData").click(function(){
     myArray=[];
+    //to avoid pushing again
    
-    console.log(localStorage.key(9));
-    for (var i=0; i<localStorage.length;i++){
+         for (var i=0; i<localStorage.length;i++){
         console.log(localStorage.key(i));
         myArray.push((localStorage.key(i)))
 
 
+    
     }
-
     extract(myArray.sort());
 
 
     
 })
 function extract(params){
-    $('#title').html("Summary of Total Vaccine Doses");
+    var l = ($('li').length);
+    //only add data if list item length to prevent readding
+    if (l==0){
+        $('#title').html("Summary of Total Vaccine Doses Administered");
     $.each(params,(index,user)=>{
-        //works but uses with index
-//        $('#data').append("<li>"+(user)+"<button id='" + ('test'+index) + "' class='vaccine'>Details</button><span></span>"+"</li>");
-//<span id='" + ('t'+index) + "' ></span>"
-$('#data').append("<li>"+(user)+" <br/><span id='" + (user) + "' class='vaccine'>Details</span>"+"</li>");
-   $(".vaccine").css("color", "red");
+       
+   $('#data').append("<li>"+(user)+" <br/><span id='" + (user) + "' class='vaccine'>Details</span>"+"</li>");
+   $(".vaccine").css("color", "brown");
+   $(".vaccine").css("font-weight", "bold");
+   $(".vaccine").css("font-size", "18px");
+   $(".vaccine").css("border", "2px dotted brown");
+   $(".vaccine").css("border-radius", "8px ");
+   $(".vaccine").css("background-color", "grey");
+   $(".vaccine").css("text-decoration", "underline");
+
 })
+    }
+    
 
 }
+
 $('#data').on("click", ".vaccine",function(){
    //check length to see if data has been appended before repeat
    //before appending because it keeps appending
    var b=$(this).attr("id");//id of span
    //check the value in span to see if has h2 tag with id=addo
    if ($('#'+b).find('#addo').length==0){
-     $('#'+b).append("<br/><h2 id='addo'>Vaccine Doses Detail sECTION</h2>");
-
-    $('#'+b).append("<br/><p>"+localStorage.getItem(b)+"</p>");
-    $("p").css("color", "orange");
-    $("p").css("text-decoration", "none");
+     $('#'+b).append("<h2 id='addo'>Vaccine Doses Detail Information</h2>");
+    var change = JSON.parse(localStorage.getItem(b));
+    $('#'+b).append("<br/><p id='cov'>"+ "ID: "+ change.id+"<br>"+ "Date: "+ change.d + 
+    "<br> Previous doses: "+change.prev_doses + "<br> Administered doses: "+change.admin+
+    "<br> Total doses fully vaccinated: "+change.fully + "<br> Total vaccinated individuals: "+
+    change.indiv+"</p>");
+   // $("p").css("color", "orange");
+   // $("p").css("text-decoration", "none");
    }
  
   // var f = $('span#'+b).next().length;
